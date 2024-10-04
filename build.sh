1ddcd56bb3b002e63a1036cfda65195ae759d4e0
AlexeyAtroshenko <alexey@exads.com>
2024-10-04 11:58:36 +0100

    avro can be generated too

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-rm -rf generated/go/*
-rm -rf generated/php/*
+rm -r generated/go/*
+rm -r generated/php/*
+rm -r generated/avro/*
 
 find ./proto_files/ -iname "*.proto" | while read file
 do
-  ./protoc/bin/protoc --php_out=./generated/php/ --go_out=./generated/go/ "$file";
+#  ./protoc/bin/protoc --php_out=./generated/php/ --go_out=./generated/go/ "$file";
+  ./protoc/bin/protoc --php_out=./generated/php/ --go_out=./generated/go/ --avro_out=./generated/avro/ --avro_opt=namespace_map=main:exads.avro "$file";
 done
--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 protobuf compiler https://github.com/protocolbuffers/protobuf
+
+protobuf to avro https://github.com/flipp-oss/protoc-gen-avro
